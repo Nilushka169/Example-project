104ec7fe4bf40a423d481c8796f0bc5f409bb4c7
Nilushka169 <s22009961@ousl.lk>
2023-12-22 18:32:24 +0530

    ganin

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './Contact.css';
+import { motion } from 'framer-motion';
 
 const Contact = () => {
 
     return (
         <div className='contact_container'>
-        <form id="Contact">
+        <form id="Contact" className='contact_form'>
+            <div className='test1'>
             <h1 >Contact <span>Here</span></h1>
             <input type="text" name="name" id="" placeholder='Enter Name' />
             <input type="email" name="email" id="" placeholder='example@email.com'/>
             <input type="phone" name="phone" id="" placeholder='+94'/>
             <textarea name='message' id='' cols={30} rows={10} placeholder='type here...'/>
             <button type="submit">send</button>
+            </div>
         </form>
+            <div style={{ position: "relative" }}>
+                <motion.div
+                    className="contact__image"
+                    initial={{ opacity: 0, x: 100 }}
+                    whileInView={{ opacity: 1, x: 0 }}
+                    //animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: 0.2, duration: 1 }}
+                >
+                    <img src='./contact4.png' className='contact_img'/>
+                </motion.div>
+                </div>
         </div>
     )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ function Header() {
         <div className="h-menu flexCenter">
           <a href="">Home</a>
           <a href="">About us</a>
-          <Link to="Contact" spy={true} smooth={true} duration={700}>Contact</Link>
+          <Link to="Contact" spy={true} smooth={true} offset={-40} duration={700}>Contact</Link>
           <Link to="Banner" spy={true} smooth={true} offset={50} duration={700}>Banner</Link>
           <a href="">Get started</a>
         </div>
