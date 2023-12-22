7824a439f91d7ee5c65bf8a4c20c5762614e418b
hashnthapramod <hashantha1232@gmail.com>
2023-12-22 17:50:42 +0530

    pramod

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,14 @@ import {Link} from "react-scroll";
 function Header() {
   return (
     <section className="h-wrapper">
-      <div className="h-container flexCenter paddings innerWidth">
+      <div className="flexCenter paddings innerWidth h-container">
         <img src="./logo.png" alt="logo" width={100} />
 
         <div className="h-menu flexCenter">
           <a href="">Home</a>
           <a href="">About us</a>
-          <Link to="Contact" spy={true} smooth={true} duration={700}>Contact</Link>
-          <Link to="Banner" spy={true} smooth={true} offset={50} duration={700}>Banner</Link>
+          <Link to="Contact" spy={true} smooth={true} duration={700}> Contact </Link>
+          <Link to="Banner" spy={true} smooth={true} offset={50} duration={700}> Banner </Link>
           <a href="">Get started</a>
         </div>
       </div>
