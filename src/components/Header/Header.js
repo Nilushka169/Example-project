becae05f61296eb22d1000eb6c80389908b56b2e
hashnthapramod <hashantha1232@gmail.com>
2023-12-22 15:41:50 +0530

    heder fix kra

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./Header.css";
+import {Link} from "react-scroll";
 
 
 function Header() {
   return (
-    
     <section className="h-wrapper">
-      <div className="h-container flexCenter paddings innerWidth ">
-        <img src="./logo.png" alt="logo"  width={100}/>
+      <div className="h-container flexCenter paddings innerWidth">
+        <img src="./logo.png" alt="logo" width={100} />
 
-        <div className="h-menu">
+        <div className="h-menu flexCenter">
           <a href="">Home</a>
           <a href="">About us</a>
-          <a href="">Banner</a>
+          <Link to="Banner" spy={true} smooth={true} offset={50} duration={700}>Banner</Link>
           <a href="">Contact Us</a>
           <a href="">Get started</a>
         </div>
