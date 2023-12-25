66bd8e9d20b33d84c57bc9a1ade8810f2d9f784b
Nilushka169 <s22009961@ousl.lk>
2023-12-25 13:19:13 +0530

    our project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,10 +11,10 @@ function Header() {
 
         <div className="h-menu flexCenter">
           <a href="">Home</a>
+          <a href="">Our projects</a>
           <a href="">About us</a>
           <Link to="Contact" spy={true} smooth={true} offset={-40} duration={700}>Contact</Link>
           <Link to="Banner" spy={true} smooth={true} offset={50} duration={700}>Banner</Link>
-          <a href="">Our projects</a>
         </div>
       </div>
     </section>
